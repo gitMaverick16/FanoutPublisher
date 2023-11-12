299c83b7d55bf4be9530d7842b460c8c057619ce
Sergio Rondon <sergio19980816@gmail.com>
2023-11-12 17:42:18 -0500

    Agregar archivos de proyecto.

---
+++ FanoutConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+IConnection conn;
+IModel channel;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+conn = factory.CreateConnection();
+channel = conn.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+
+var consumerTag =  channel.BasicConsume("my.queue1", true, consumer);
+
+Console.WriteLine("Waiting for messages. Press any key to exit.");
+Console.ReadKey();
+
+
+static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+{
+    string message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Message: " + message);
+
+    //channel.BasicAck(e.DeliveryTag, false);
+    //channel.BasicNack(e.DeliveryTag, false, true);
+}
---
+++ FanoutPublisher/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+IConnection conn;
+IModel channel;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+conn = factory.CreateConnection();
+channel = conn.CreateModel();
+
+channel.ExchangeDeclare("ex.fanout", "fanout", true, false, null);
+
+channel.QueueDeclare("my.queue1", true, false, false, null);
+channel.QueueDeclare("my.queue2", true, false, false, null);
+
+channel.QueueBind("my.queue1", "ex.fanout", "");
+channel.QueueBind("my.queue2", "ex.fanout", "");
+
+channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 1"));
+channel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 2"));
+
+Console.WriteLine("Press a key to exit.");
+Console.ReadKey();
+
+channel.QueueDelete("my.queue1");
+channel.QueueDelete("my.queue2");
+channel.ExchangeDelete("ex.fanout");
+
+channel.Close();
+conn.Close();
